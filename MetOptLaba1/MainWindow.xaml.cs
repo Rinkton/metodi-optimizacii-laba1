2fe9cf223e834ef7fa369ba212e3aca9434424f0
Rink <bystrova1979inbox.ru@gmail.com>
2025-11-07 19:33:59 +0300

    Made modification of the tables when changing variable and constraint count

--- MetOptLaba1/MainWindow.xaml.cs
+++ MetOptLaba1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Data.Common;
 using System.Linq;
 using System.Text;
@@ -27,13 +28,78 @@ namespace MetOptLaba1
 
         private void grid_Loaded(object sender, RoutedEventArgs e)
         {
-            target_func_grid.ItemsSource = new List<aba> { new aba { Id = 1, Name = "aba" }, new aba { Id = 2, Name = "ABAaaaaaaaaaaaaaaaaaaa" } };
+        }
+
+        private void variableAmount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateTables();
+        }
+
+        private void constraintAmount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateTables();
+        }
+
+        private void updateTables()
+        {
+            if(targetGrid == null) {
+                return;
+            }
+            try {
+                int columnCount = int.Parse(variableAmount.Text);
+                int rowCount = int.Parse(constraintAmount.Text);
+                updateTargetTable(columnCount);
+                updateConstraintTable(columnCount, rowCount);
+            }
+            catch(FormatException ex) {
+                return;
+            }
+        }
+
+        private void updateTargetTable(int columnCount)
+        {
+            DataTable dt = new DataTable();
+
+            for(int i = 1; i <= columnCount; i++) {
+                dt.Columns.Add($"c{i}", typeof(string));
+            }
+
+            dt.Columns.Add($"c", typeof(string));
+
+            var row = dt.NewRow();
+            for(int i = 1; i <= columnCount; i++) {
+                row[$"c{i}"] = "";
+            }
+            row[$"c"] = "";
+            dt.Rows.Add(row);
+
+            targetGrid.ItemsSource = dt.DefaultView;
+            targetGrid.AutoGenerateColumns = true;
+        }
+
+        private void updateConstraintTable(int columnCount, int rowCount)
+        {
+            DataTable dt = new DataTable();
+
+            for(int i = 1; i <= columnCount; i++) {
+                dt.Columns.Add($"a{i}", typeof(string));
+            }
+
+            dt.Columns.Add($"b", typeof(string));
+
+            for(int j = 1; j <= rowCount; j++) {
+                var row = dt.NewRow();
+                for(int i = 1; i <= columnCount; i++) {
+                    row[$"a{i}"] = "";
+                }
+                row[$"b"] = "";
+                dt.Rows.Add(row);
+            }
+
+            constraintGrid.ItemsSource = dt.DefaultView;
+            constraintGrid.AutoGenerateColumns = true;
         }
     }
 
-    class aba
-    {
-        public int Id {  get; set; }
-        public string Name { get; set; }
-    }
+    // TODO: Лаба на решение граф методом целочисл можно перебором программно
 }
